5fa8c2420a2d022f18f54ecd318c8f41869a8f58
Clery123 <56003762+Clery123@users.noreply.github.com>
2022-07-08 11:19:24 +0200

    Update Form1.cs

--- PresentationLayer/Form1.cs
+++ PresentationLayer/Form1.cs
@@ -18,38 +18,10 @@ namespace PresentationLayer
     {
         public Form1()
         {
-            //Employee emp = EmployeeTable.SelectID(5);
-           // Schedule veh = ScheduleTable.SelectID(1);
 
-            //Driver driver = DriverTable.SelectID(emp.dID);
-            //Console.WriteLine(driver.Hours_worked.ToString());
-           // Console.WriteLine ("Denny nájazd KM: "+DriverTable.Daily(driver.dID).ToString());
-           // Collection<Vehicle> vehi = DriverTable.Added_vehicles(1);
-            //String s = "";
-            /*foreach (Vehicle ve in vehi)
-            {
-                s = s + ve.Name+"\n";
-               
-            }*/
-           // Console.WriteLine("Pridelene vozidla: \n" + s);
-            //Console.WriteLine(VehicleTable.VehicleCheck());
-
-
-           /* Vehicle veh = VehicleTable.Select()[0];
-            Collection<Vehicle> vehicles = new Collection<Vehicle>();
-            vehicles=VehicleTable.Select(db);*/
             
             InitializeComponent();
-            
-           // db.Close();
-            /*if (con == true)
-            {
-                label1.Text = "Con.";
-                foreach (Vehicle v in vehicles)
-                {
-                    label2.Text += v.Type.ToString();
-                }
-            }*/
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -76,7 +48,6 @@ namespace PresentationLayer
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //EmployeeTable.Bonus();
         }
 
         private void button4_Click(object sender, EventArgs e)
